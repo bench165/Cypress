63ea595f0d5fa073df172609a4d696ee2391b772
mew0205 <64160165@go.buu.ac.th>
2023-10-03 17:23:01 +0700

    dashboard

--- cypress/e2e/dashboard.feature
+++ cypress/e2e/dashboard.feature
@@ -1,13 +1,63 @@
 Feature: dashboard
-
-    Feature Login page will work depending on the user credentials.
+    Feature dashboard displays electricity consumption data as a data card, day, month, year, and electricity consumption graph.
 
 Background:
     Given Open the IoT Connect web browser
+    When Provide valid "ProjectIOT" and "IOT@1234"
+    And Click on submit button
+    
 
-    Scenario: Testing access to the history display screen
-        When Provide valid "ProjectIOT" and "IOT@1234"
-        And Click on submit button  
-        And Click the History Power Moniter menu bar
-        Then Successfully entered the history screen
-        
+# Scenario: Testing access to the Dashboard screen
+#     Given Click the IoT Connect menu bar
+#     Then Successfully entered the Dashboard screen
+
+# Scenario: Dashboard screen display
+#     Given Click the IoT Connect menu bar
+#     Then Successfully Dashboard screen display
+
+Scenario: Test the day button
+    Given Click the IoT Connect menu bar
+    And Click the day button
+    Then Successfully entered the day button
+
+# Scenario: Test the month button
+#     Given Click the IoT Connect menu bar
+#     And Click the month button
+#     Then Successfully entered the month button
+
+# Scenario: Test the year button
+#     Given Click the IoT Connect menu bar
+#     And Click the year button
+#     Then Successfully entered the year button
+
+
+
+# 1. `cy.visit()`: ใช้เพื่อเข้าชมหน้าเว็บที่คุณต้องการทดสอบ.
+
+# 2. `cy.get()`: ใช้เพื่อเลือก HTML elements บนหน้าเว็บเพื่อทดสอบ.
+
+# 3. `cy.click()`: ใช้เพื่อคลิกที่ elements ที่ถูกเลือก.
+
+# 4. `cy.type()`: ใช้เพื่อป้อนข้อความลงใน elements เช่น input fields.
+
+# 5. `cy.contains()`: ใช้เพื่อค้นหา elements ที่มีข้อความหรือเนื้อหาที่ระบุ.
+
+# 6. `cy.should()`: ใช้เพื่อทำการตรวจสอบเงื่อนไขต่าง ๆ บน elements เช่นการตรวจสอบข้อความ, คลาส, หรือคุณสมบัติอื่น ๆ.
+
+# 7. `cy.intercept()`: ใช้เพื่อสร้างการจำลองการเรียก API และควบคุมการตอบสนองของ API เพื่อทดสอบการประสิทธิภาพของแอปพลิเคชัน.
+
+# 8. `cy.route()`: คำสั่งที่เก่าและถูกแทนที่ด้วย `cy.intercept()` ใน Cypress 6 ขึ้นไป.
+
+# 9. `cy.exec()`: ใช้สำหรับการรันคำสั่งเป็นคำสั่งใน shell (Command Line) และจัดการกับกระบวนการภายนอก.
+
+# 10. `cy.scrollTo()`: ใช้เพื่อเลื่อนหน้าเว็บไปยังตำแหน่งที่ระบุ.
+
+# 11. `cy.wait()`: ใช้สำหรับการรอคอยเพื่อรอให้เหตุการณ์หรือเงื่อนไขเกิดขึ้น.
+
+# 12. `cy.its()`: ใช้สำหรับการเข้าถึงคุณสมบัติของ elements และทำการตรวจสอบ.
+
+# 13. `cy.invoke()`: ใช้สำหรับการเรียกใช้ฟังก์ชันหรือเมทอดบน elements และทำการตรวจสอบ.
+
+# 14. `cy.fixture()`: ใช้สำหรับการโหลดข้อมูลจาก fixture files เพื่อใช้ในการทดสอบ.
+
+# 15. `cy.route2()`: ใช้สำหรับการจำลองการเรียก API ใน Cypress 7 ขึ้นไป.
--- cypress/e2e/dashboard/dashboard.js
+++ cypress/e2e/dashboard/dashboard.js
@@ -1,5 +1,7 @@
 import { Given, When, Then, And } from 'cypress-cucumber-preprocessor/steps';
 
+// import rgbHex from 'rgb-hex';
+
 Cypress.on('uncaught:exception', (err, runnable) => {
   // returning false here prevents Cypress from
   // failing the test
@@ -22,14 +24,71 @@ And('Click on submit button', () => {
   cy.get('#btn_login').click();
 });
 
-// คลิกเมนู History Power Monitor
-And('Click the History Power Moniter menu bar', () => {
+// คลิกเมนู Dashboard Power Monitor
+Given('Click the IoT Connect menu bar', () => {
   cy.wait(4000);
-  cy.get(':nth-child(4) > .ttt-menu-bg').click();
-  cy.get('.menu-open > .nav > :nth-child(2) > .nav-link').click();
+  cy.get(':nth-child(2) > .ttt-menu-bg').click();
+  cy.get(':nth-child(2) > .nav > :nth-child(2) > .nav-link').click();
 });
 
-// ตรวจสอบว่าหัวข้อเรื่องตรงไหม
-Then('Successfully entered the history screen', () => {
-  cy.get('h1').contains('History Power Monitor');
+// ตรวจสอบว่าตัวพื้นหลังแสดงหรือไม่
+Then('Successfully entered the Dashboard screen', () => {
+  cy.get('h1').should('be.visible');
 });
+
+// ตรวจสอบว่ามีการแสดงผลของแต่ละส่วนหรือไม่
+Then('Successfully Dashboard screen display', () => {
+  cy.get('h1').contains('Dashboard Power Monitor');
+  cy.get(':nth-child(1) > .card-monitor > .col-span-3 > .monitor_card > .card-value').should('be.visible');
+  cy.get(':nth-child(2) > .card-monitor > .col-span-3 > .monitor_card > .card-value').should('be.visible');
+  cy.get(':nth-child(3) > .card-monitor > .col-span-3 > .monitor_card > .card-value').should('be.visible');
+  cy.get(':nth-child(3) > .card-monitor > .col-span-3 > .monitor_card > .card-value').should('be.visible');
+  cy.get('.first').should('be.visible');
+  cy.get('.multi-button > :nth-child(2)').should('be.visible');
+  cy.get('.third').should('be.visible');
+  cy.get('#main').should('be.visible');
+  cy.get('#power').should('be.visible');
+  cy.get('#energy').should('be.visible');
+  cy.get('#voltage').should('be.visible');
+  cy.get('#frequency').should('be.visible');
+})
+
+const hexToRgb = (hex) => {
+  const rValue = ParseInt(hex.substring(0, 2), 16);
+  const gValue = ParseInt(hex.substring(2, 4), 16);
+  const bValue = ParseInt(hex.substring(4), 16);
+  return `rgb(${rValue}, ${gValue}, ${bValue})`;
+}
+
+
+
+//ทดสอบการทำงานปุ่ม Day
+And('Click the day button', () => {
+  cy.get('.first').click();
+})
+
+//เช็คสีของปุ่ม Day
+Then('Successfully entered the day button', () => {
+  // cy.get('.first').invoke('css', 'background-color').then((bgcolor) => {expect(rgbHex(bgcolor)).to.eq('4bdd33')})
+  cy.get('.first').should('have.css', 'background-color', hexToRgb('3579f6'))
+})
+
+//ทดสอบการทำงานปุ่ม Month
+And('Click the month button', () => {
+  cy.get('.multi-button > :nth-child(2)').click();
+})
+
+//เช็คสีของปุ่ม Month
+Then('Successfully entered the month button', () => {
+  cy.get('.multi-button > :nth-child(2)').should('eq', 'reg(#3579f6)')
+})
+
+//ทดสอบการทำงานปุ่ม Year
+And('Click the year button', () => {
+  cy.get('.third').click();
+})
+
+//เช็คสีของปุ่ม Year
+Then('Successfully entered the year button', () => {
+  cy.get('.third').should('eq', '#3579f6')
+})
